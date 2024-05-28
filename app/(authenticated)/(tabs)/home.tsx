bd0a2d02858378fc6e8cc24125ab3658c0537cb9
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-05-29 01:31:44 +0300

    feat: balance store managing with zustand & mmkv

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,30 +1,77 @@
 import Dropdown from '@/components/Dropdown';
 import RoundButton from '@/components/RoundButton';
 import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useBalanceStore } from '@/store/balanceStore';
+import { Ionicons } from '@expo/vector-icons';
 import { View, StyleSheet, ScrollView, Text } from 'react-native';
 
 const Home = () => {
-  const balance = 1420;
+  const { balance, clearTransactions, runTransaction, transactions } =
+    useBalanceStore();
   const currency = '₺';
 
   const onAddMoney = () => {
-    // pass
+    runTransaction({
+      id: Math.random().toString(),
+      amount: Math.floor(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1),
+      date: new Date(),
+      title: 'Added money',
+    });
   };
 
   return (
     <ScrollView style={{ backgroundColor: Colors.background }}>
       <View style={styles.account}>
         <View style={styles.row}>
-          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.balance}>{balance()}</Text>
           <Text style={styles.currency}>{currency}</Text>
         </View>
       </View>
 
       <View style={styles.actionRow}>
-        <RoundButton icon={'add'} onPress={() => {}} text='Add money' />
-        <RoundButton icon={'refresh'} text='Exchange' />
+        <RoundButton icon={'add'} onPress={onAddMoney} text='Add money' />
+        <RoundButton
+          icon={'refresh'}
+          onPress={clearTransactions}
+          text='Exchange'
+        />
         <RoundButton icon={'list'} text='Details' />
         <Dropdown />
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View style={styles.transactions}>
+        {transactions.length === 0 && (
+          <Text style={{ padding: 14, color: Colors.gray }}>
+            No transactions yet
+          </Text>
+        )}
+        {transactions.map((transaction) => (
+          <View
+            key={transaction.id}
+            style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}
+          >
+            <View style={styles.circle}>
+              <Ionicons
+                name={transaction.amount > 0 ? 'add' : 'remove'}
+                size={24}
+                color={Colors.dark}
+              />
+            </View>
+
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontWeight: '400' }}>{transaction.title}</Text>
+              <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                {transaction.date.toLocaleDateString()}
+              </Text>
+            </View>
+            <Text>
+              {transaction.amount}
+              {currency}
+            </Text>
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
@@ -51,6 +98,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 20,
+  },
+  transactions: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    gap: 20,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.lightGray,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 export default Home;
